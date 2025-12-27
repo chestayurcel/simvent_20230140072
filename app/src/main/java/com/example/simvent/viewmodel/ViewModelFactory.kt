a780e3cb06b2009830d45a67bab0394cc49a3da8
Yurcell <chestayurcel@gmail.com>
2025-12-27 23:55:06 +0700

    Menghapus impor yang tidak digunakan di ViewModelFactory.kt

--- app/src/main/java/com/example/simvent/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/example/simvent/viewmodel/ViewModelFactory.kt
@@ -1,12 +1,10 @@
 package com.example.simvent.viewmodel
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.simvent.SimventApplication
-import com.example.simvent.di.AppContainer
 
 object ViewModelFactory {
     val Factory = viewModelFactory {
