be244a34bb87c8af762ab7d6202b097126731c51
Yurcell <chestayurcel@gmail.com>
2025-12-28 01:38:30 +0700

    Membuat file NavigationMap.kt untuk menangani navigasi antar layar

---
+++ app/src/main/java/com/example/simvent/uicontroller/NavigationMap.kt
@@ -0,0 +1,68 @@
+package com.example.simvent.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.simvent.uicontroller.route.Screen
+import com.example.simvent.view.AssetListScreen
+import com.example.simvent.view.DashboardScreen
+import com.example.simvent.view.LoginScreen
+
+@Composable
+fun NavigationMap(navController: NavHostController) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+
+        // 1. RUTE LOGIN
+        composable(Screen.Login.route) {
+            LoginScreen(
+                onLoginSuccess = {
+                    // Pindah ke Dashboard, dan HAPUS riwayat Login
+                    navController.navigate(Screen.Dashboard.route) {
+                        popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        // 2. RUTE DASHBOARD
+        composable(Screen.Dashboard.route) {
+            DashboardScreen(
+                onNavigateToAssets = {
+                    navController.navigate(Screen.AssetList.route)
+                },
+                onNavigateToRooms = {
+                    // Nanti: navController.navigate(Screen.RoomList.route)
+                },
+                onLogout = {
+                    // Kembali ke Login, hapus semua riwayat backstack
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(0)
+                    }
+                }
+            )
+        }
+
+        // 3. RUTE ASSET LIST
+        composable(Screen.AssetList.route) {
+            AssetListScreen(
+                onLogout = {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(0)
+                    }
+                }
+            )
+        }
+
+        // 4. RUTE ROOM LIST
+        /*
+        composable(Screen.RoomList.route) {
+             RoomListScreen(...)
+        }
+        */
+    }
+}
