d7245e95b27094a71a21bb55bf6906122649c05f
Yurcell <chestayurcel@gmail.com>
2025-12-28 23:35:12 +0700

    Membuat file RoomViewModel.kt untuk menangani logika pengambilan data ruangan

---
+++ app/src/main/java/com/example/simvent/viewmodel/RoomViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.simvent.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simvent.data.model.RoomItem
+import com.example.simvent.data.repository.AuthRepository
+import com.example.simvent.data.repository.RoomRepository
+import kotlinx.coroutines.launch
+
+sealed interface RoomUiState {
+    object Loading : RoomUiState
+    data class Success(val rooms: List<RoomItem>) : RoomUiState
+    data class Error(val message: String) : RoomUiState
+}
+
+class RoomViewModel(
+    private val roomRepository: RoomRepository,
+    private val authRepository: AuthRepository
+) : ViewModel() {
+
+    var uiState: RoomUiState by mutableStateOf(RoomUiState.Loading)
+        private set
+
+    init {
+        getRooms()
+    }
+
+    fun getRooms() {
+        viewModelScope.launch {
+            uiState = RoomUiState.Loading
+            val token = authRepository.getSessionToken()
+
+            if (token != null) {
+                val result = roomRepository.getRooms(token)
+                uiState = if (result.isSuccess) {
+                    val rooms = result.getOrNull()?.data ?: emptyList()
+                    RoomUiState.Success(rooms)
+                } else {
+                    RoomUiState.Error(result.exceptionOrNull()?.message ?: "Gagal memuat ruangan")
+                }
+            } else {
+                uiState = RoomUiState.Error("Sesi habis")
+            }
+        }
+    }
+}
