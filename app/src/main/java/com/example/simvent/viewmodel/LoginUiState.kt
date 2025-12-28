1b38266f4d013d581a6f5d0cf73ca3dacd8e6f24
Yurcell <chestayurcel@gmail.com>
2025-12-28 23:58:37 +0700

    Menghapus file LoginUiState.kt

--- app/src/main/java/com/example/simvent/viewmodel/LoginUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.simvent.viewmodel
-
-sealed interface LoginUiState {
-    object Idle : LoginUiState          // Status awal
-    object Loading : LoginUiState       // Sedang memutar loading spinner
-    data class Success(val message: String) : LoginUiState // Login berhasil
-    data class Error(val message: String) : LoginUiState   // Login gagal
-}
